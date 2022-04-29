ab2bb9c2fcf32a4b344f76c0552ec98c93da7d0c
Kuturga Valentin <kvv095@yandex.ru>
2022-04-29 21:13:03 +0300

    тест с промисами

--- script.js
+++ script.js
@@ -1,61 +1,51 @@
+const makeGETRequest = new Promise(() => {}) 
+function makeGETRequest(url, callback) {
+  var xhr;
+  if (window.XMLHttpRequest) {
+  xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+  xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+  callback(xhr.responseText);
+  }
+  }
+  xhr.open('GET', url, true);
+  xhr.send();
+  }
+
+const API_URL =
+'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+
 class GoodsItem {
-  constructor (title,price) {
-    this.title = title;
+  constructor (product_name, price){
+    this.product_name =product_name;
     this.price = price;
   }
-  render () {
-    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
-  }
-}
-
-class GoodsList {
-  constructor () {
-    this.goods = [];
-  }
-  fetchGoods() {
-    this.goods = [
-      { title: 'Shirt', price: 150 },
-      { title: 'Shoes', price: 250 },
-      { title: 'Socks', price: 50 },
-      { title: 'Jacket', price: 350 },
-    ];  
-  }  
   render() {
-    let listHtml = '';
-    this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.title, good.price);
-      listHtml += goodItem.render();
-    });
-    document.querySelector('.goods-list').innerHTML = listHtml;
+  return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
   }
-  sumItem() {
-    let sumItems = 0;
-    for (let i =0; i < this.goods.length; i++) {
-      sumItems += this.goods[i].price;
-    } 
-    document.querySelector('.itogPrice').innerHTML = `Сумма: ${sumItems}`;
-  }    
-}
-
-class Basket {
-  constructor() {
   }
-  clear(){}
-  countItems() {}
-  sumPrice() {}
-  pay() {}
+  class GoodsList {
+  // ...
+  fetchGoods(cb) {
+    makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+    this.goods = JSON.parse(goods);
+    cb();
+    })
+    }
+  render() {
+  let listHtml = '';
+  this.goods.forEach(good => {
+  const goodItem = new GoodsItem(good.product_name, good.price);
+  listHtml += goodItem.render();
+});
+document.querySelector('.goods-list').innerHTML = listHtml;
 }
-class ItemBasket {
-  constructor() { 
-  }
-  outputTitle() {}
-  add() {}
-  del() {}
 }
-
 const list = new GoodsList();
-list.fetchGoods();
+list.fetchGoods(() => {
 list.render();
-list.sumItem();
-
-
+});
